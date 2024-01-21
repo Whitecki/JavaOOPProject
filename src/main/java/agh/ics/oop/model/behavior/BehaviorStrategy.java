52f45a7cd7d4f88ecb0fe44171b27f44ccc2d054
helenaszczepanowska <helenaszczepanowska@gmail.com>
2024-01-21 21:07:33 +0100

    fix active gen

--- src/main/java/agh/ics/oop/model/behavior/BehaviorStrategy.java
+++ src/main/java/agh/ics/oop/model/behavior/BehaviorStrategy.java
@@ -3,7 +3,8 @@ import agh.ics.oop.model.animal.Animal;
 import agh.ics.oop.model.animal.Vector2D;
 
 import java.util.HashMap;
+import java.util.List;
 
 public interface BehaviorStrategy {
-    void performAction(HashMap<Vector2D, Animal> animalHashMap);
+    void performAction(HashMap<Vector2D, List<Animal>> animalHashMap);
 }
